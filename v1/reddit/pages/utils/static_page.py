398a40c49e6fa14922225968770064d5415a7398
dfresco <unconfigured@null.spigotmc.org>
2023-08-23 08:10:25 -0600

    added type hints for lsp

--- v1/reddit/pages/utils/static_page.py
+++ v1/reddit/pages/utils/static_page.py
@@ -13,26 +13,30 @@ class StaticPage(Page):
     def __init__(self, driver, *args, **kwargs):
         Page.__init__(self, driver)
 
-    def _get_url(self):
-        pass
+    def _get_url(self) -> str:
+        return ""
 
-    def _get_next_sig(self):
-        pass
+    def _get_next_sig(self) -> str:
+        return ""
 
-    def _get_next_edges(self, data):
+    def _get_next_edges(self, data) -> list:
         for key in self._get_next_sig().split("."):
             data = data[key]
         return [edge["node"] for edge in data]
 
-    def _get_first_page_data(self):
+    def _get_first_page_data(self) -> dict:
         body_text = get_target_request_body(
             self._driver, url_regex=f"^{self._get_url()}$")
         data_elem = html.fromstring(body_text).xpath("//script[@id='data']")[0]
-        data_text = re.match(r"window\.___r = (.*);", data_elem.text).group(1)
+        match = re.match(r"window\.___r = (.*);", data_elem.text)
+        if match is None:
+            return {}
+        else:
+            data_text = match.group(1)
         data = json.loads(data_text)
         return data
 
-    def first_page(self):
+    def first_page(self) -> list:
         self._driver.get(self._get_url())
         data = self._get_first_page_data()
         post_data = []
@@ -61,17 +65,19 @@ class StaticPage(Page):
             post_data.append(post_obj)
         return post_data
 
-    def _get_next_page_data(self):
+    def _get_next_page_data(self) -> dict:
         while len(self.get_elems("//div[@data-testid='post-container']")) < 25:
             time.sleep(0.5)
         del self._driver.requests
         self.scroll_to_bottom()
         body_text = get_target_request_body(
             self._driver, response_sig=self._get_next_sig())
+        if body_text is None:
+            return {}
         data = json.loads(body_text)
         return data
 
-    def next_page(self):
+    def next_page(self) -> list:
         data = self._get_next_page_data()
         post_data = []
         for post in self._get_next_edges(data):
